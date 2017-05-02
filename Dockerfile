af21ffbde937b8bd9f4ff75d0ccbe1810b46e037
William Buchwalter <wbuchwalter@gmail.com>
2017-05-02 16:40:23 -0700

    prediction base

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,4 @@ WORKDIR /app
 COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r ./requirements.txt
 COPY ./ /app
-
-
-
+RUN chmod +x runs.sh
---
+++ runs.sh
@@ -0,0 +1,2 @@
+mkdir ./checkpoint/sunny-rainy-city-lr00002-run1
+python cyclegan.py -t 180 -c ./checkpoint/sunny-rainy-city-lr00002-run1/model.ckpt -l 0.00002
