28bf67156817b4844961dd1a47989cea773e0bbf
William Buchwalter <wbuchwalter@gmail.com>
2017-05-03 09:28:17 -0700

    modify runs

--- runs.sh
+++ runs.sh
@@ -1,2 +1,7 @@
-mkdir ./checkpoint/sunny-rainy-city-lr00002-run1
-python cyclegan.py -t 180 -c ./checkpoint/sunny-rainy-city-lr00002-run1/model.ckpt -l 0.00002
+mkdir ./checkpoint/sunny-rainy-beach-lr00002-run1
+mkdir ./checkpoint/sunny-rainy-beach-lr0002-run1
+mkdir ./checkpoint/sunny-rainy-beach-lr002-run1
+python cyclegan.py -t 180 -c ./checkpoint/sunny-rainy-beach-lr00002-run2/model.ckpt -l 0.00002 -sl 1
+python cyclegan.py -t 180 -c ./checkpoint/sunny-rainy-beach-lr0002-run2/model.ckpt -l 0.0002 -sl 1
+python cyclegan.py -t 180 -c ./checkpoint/sunny-rainy-beach-lr002-run2/model.ckpt -l 0.002 -sl 1
+
